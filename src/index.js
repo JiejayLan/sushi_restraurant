bf2c82e75794b15ab7e7dc1e443bd2ee80c5e26b
Jie Lan <lanjie3458@gmail.com>
2019-01-02 12:22:23 -0500

    route

--- src/index.js
+++ src/index.js
@@ -5,10 +5,43 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
 import App from './App';
-
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <App /> , document.getElementById('root'));
+
+
+const MainPage = () => (
+      <App/>
+);
+
+const Menu = () => (
+    <div>
+        This is menu
+    </div>
+);
+
+// const EditExpensePage = () => (
+//     <div>
+//         This is from my edit expense component
+//     </div>
+// );
+//
+const HelpPage = () => (
+    <div>
+        This is from my help component
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={MainPage} exact={true} />
+            <Route path="/menu" component={Menu} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render( routes , document.getElementById('root'));
 
 registerServiceWorker();
